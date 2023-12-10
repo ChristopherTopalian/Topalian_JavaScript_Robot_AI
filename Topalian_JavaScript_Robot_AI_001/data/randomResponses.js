ec9ad09d08d07b090c2ebbc0f49dca7c400455bd
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2023-12-10 13:59:58 -0500

    Add files via upload

---
+++ Topalian_JavaScript_Robot_AI_001/data/randomResponses.js
@@ -0,0 +1,23 @@
+// Dedicated to God the Father
+
+// All Rights Reserved Christopher Andrew Topalian Copyright 2000-2023
+
+// https://github.com/ChristopherTopalian
+
+// https://github.com/ChristopherAndrewTopalian
+
+// randomResponses.js
+
+// if no keywords are found, use these responses
+let randomResponses =
+[
+    `That's interesting.`,
+    `Would you tell me more?`,
+    `Hmm, tell me more about that.`,
+    `Would you elaborate?`,
+    `Interesting, please go on.`,
+    `What else can you tell me about it?`,
+    `Do you have anymore information on it?`,
+    `Say again?`,
+    `Fascinating! Tell me more.`,
+];
---
+++ Topalian_JavaScript_Robot_AI_001/data/responses.js
@@ -0,0 +1,106 @@
+// Dedicated to God the Father
+
+// All Rights Reserved Christopher Andrew Topalian Copyright 2000-2023
+
+// https://github.com/ChristopherTopalian
+
+// https://github.com/ChristopherAndrewTopalian
+
+// responses.js
+
+// if keyword is found, use these responses
+let responses =
+[
+    {
+        keywords: [
+            `hi`,
+            `howdy`,
+            `hey`,
+            `holla`,
+            `good evening`,
+            `good morning`,
+            `greetings`,
+            `yo`,
+        ],
+
+        responses: [
+            `Howdy Buddy`,
+            `Hi Friend`,
+            `Hi there`,
+            `Greetings Friend`,
+            `Good day`,
+            `Bonjour`
+        ]
+    },
+
+    {
+        keywords: [
+            `what is the weather like`,
+            `what is the forecast`,
+            `is it going to be sunny`,
+        ],
+        responses: [
+            `It's a beautiful day.`,
+            `The weather is very nice.`,
+            `It's sunny and warm.`
+        ]
+    },
+
+    {
+        keywords: [
+            `how are you`,
+            `what's up`,
+            `what are you up to?`
+        ],
+        responses: [
+            `I'm doing good, you?`,
+            `Having fun and you?`,
+            `I'm good, how about you?`
+        ]
+    },
+
+    {
+        keywords: [
+            `excellent`,
+            `very good`,
+            `doing good`
+        ],
+        
+        responses: [
+            `Nice`,
+            `That's nice`,
+            `Very good`,
+            `Very Excellent`
+        ]
+    },
+
+    {
+        keywords:
+        [
+            `bye`,
+            `goodbye`,
+            `take care`
+        ],
+        responses:
+        [
+            `Bye. Talk to you soon.`,
+            `Goodbye`,
+            `Have fun`
+        ]
+    },
+
+    {
+        keywords:
+        [
+            `what are your interests`,
+            `what do you want to do as a career`,
+            `what is your current job goal`
+        ],
+        responses:
+        [
+            `Computer Science is fun.`,
+            `Programming is lots of fun.`,
+            `I like programming a lot.`
+        ]
+    }
+];
